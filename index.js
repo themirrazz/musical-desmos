3c8edd020d2a9a3c8ee5abb9fdaf6765ce57fd3e
themirrazz <89490659+themirrazz@users.noreply.github.com>
2024-01-09 09:29:38 -0800

    e

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-var MusicalDesmos = (function () {})();
+var MusicalDesmos = (function () {
+    var keyf = "f\\left(x\\right)=440\\cdot2^{\\frac{x-49}{12}}";
+    var GenerateTrack = function (track) {
+        var lines = [];
+        track.forEach(key => {
+            //
+        });
+    };
+})();

84126fa845813107949118f2dd66e2d7b015da38
themirrazz <89490659+themirrazz@users.noreply.github.com>
2024-01-09 09:50:36 -0800

    aa

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var MusicalDesmos = (function () {
+var MidiJSONToDesmos = (function () {
     var keyf = "f\\left(x\\right)=440\\cdot2^{\\frac{x-49}{12}}";
     var GenerateTrack = function (track) {
         var lines = [];
@@ -7,5 +7,14 @@ var MusicalDesmos = (function () {
                 `\\left\\{b\\ge${key.time+0.25}:\\left\\{b\\le${key.time+0.25+key.duration}:1\\right\}\\right\\}`
             );
         });
+        return lines.join("\n");
+    };
+    var MidiToDesmos = function (midi) {
+        var lines = [
+            "f\\left(x\\right)=440\\cdot2^{\\frac{x-49}{12}}",
+            "b = 0",
+            "t = "+midi.header.tempos[0].bpm
+        ];
+        midi.tracks;
     };
 })();
